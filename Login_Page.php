3a8df308b0beb1cc7263c1d999b8f73b5b15be7d
IniDey <shrik.b@hotmail.com>
2020-05-16 23:26:32 -0400

    login and register system with css

--- Login_Page.php
+++ Login_Page.php
@@ -3,33 +3,40 @@
 <html>
  <head>
   <title>Sign On</title>
+  <link rel = "stylesheet" type = "text/css" href = "style.css">
  </head>
 
   <body>
+      <img src = "logo.png" class = "logo">
    <div class = "reg">
    <h1>Let's Get Started</h1>
    <br></br>
-    <h2>Login</h2>
+   <h2>or</h2>
    </div>
-
+<div class = "inputs">
    <div class = "existuser">
+   <h2>Login</h2>
     <form method = "POST" action = "Login_Page.php">
-    <input type = "text" name = "username" value = "Username">
-    <input type = "text" name = "password1" value = "Password"><br></br>
+    <input type = "text" name = "username" placeholder = "Username">
+    <br></br>
+    <input type = "text" name = "password1" placeholder = "Password">
+    <br></br>
     <button type = "submit" name = "login" class = "btn">Next</button>
     </form>
    </div>
 <br></br>
 
-<div class = "log" >
-    <h2>Register Now</h2>
-   </div>
    <div class = "newuser">
+   <h2>Register Now</h2>
     <form method= "POST" action = "Login_Page.php">
-    <input type = "text" name = "username" value = "Enter Username">
-    <input type = "text" name = "password1" value = "Enter Password">
-    <input type = "text" name = "password2" value = "Confirm Password"><br></br>
+    <input type = "text" name = "username" placeholder = "Enter Username">
+    <br></br>
+    <input type = "text" name = "password1" placeholder = "Enter Password">
+    <br></br>
+    <input type = "text" name = "password2" placeholder = "Confirm Password">
+    <br></br>
     <button type = "submit" name = "register" class = "btn">Register</button>
     </form>
+</div>
   </body>
 </html>
