d3d9be6b0263625f5ee419233a427c01a7aedbea
Shashank .S <s.shak101@gmail.com>
2020-05-16 23:35:41 -0400

    Upload system for .docx files converting to basic text

---
+++ upload.php
@@ -0,0 +1,26 @@
+<?php
+
+$servername = "35.203.5.122";
+$username = "root";
+$password = "harshilpicopenis123";
+$db = "skiptheedits";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['submit'])) {
+	
+  $title = mysqli_real_escape_string($conn, $_POST['title']);
+  $desc = mysqli_real_escape_string($conn, $_POST['instructions']);
+  $body = mysqli_real_escape_string($conn, $_POST['body']);
+  echo $title, $desc, $body;
+	
+  $sql = "INSERT INTO essays (title, descrip, body) VALUES ('$title', '$desc', '$body')";
+  mysqli_query($conn, $sql);
+
+}
